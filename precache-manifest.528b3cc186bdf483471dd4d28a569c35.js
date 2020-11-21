f7aaf25135ef136fb54f1dc0b14a15cdbcb5813a
piotr-andrzej-bochenek <piotr8207@interia.eu>
2020-11-21 11:30:52 +0100

    Updates

---
+++ precache-manifest.528b3cc186bdf483471dd4d28a569c35.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc52fddd36617a5b334d18ebb7be93b5",
+    "url": "/jfdz12-fantasy-code-app/index.html"
+  },
+  {
+    "revision": "4a6a6896f5de3273ffc3",
+    "url": "/jfdz12-fantasy-code-app/static/css/2.2643f009.chunk.css"
+  },
+  {
+    "revision": "95e1e2dafed5999146cf",
+    "url": "/jfdz12-fantasy-code-app/static/css/main.85217fed.chunk.css"
+  },
+  {
+    "revision": "4a6a6896f5de3273ffc3",
+    "url": "/jfdz12-fantasy-code-app/static/js/2.dad816bc.chunk.js"
+  },
+  {
+    "revision": "95e1e2dafed5999146cf",
+    "url": "/jfdz12-fantasy-code-app/static/js/main.2e6881b0.chunk.js"
+  },
+  {
+    "revision": "949bb24847d5ebe6c0de",
+    "url": "/jfdz12-fantasy-code-app/static/js/runtime-main.97088eee.js"
+  },
+  {
+    "revision": "b2d0a21aadb43556e775ac9b95552c73",
+    "url": "/jfdz12-fantasy-code-app/static/media/friends.b2d0a21a.png"
+  },
+  {
+    "revision": "d9944d040ad4e901a9b55ffd5c4ca103",
+    "url": "/jfdz12-fantasy-code-app/static/media/join.d9944d04.png"
+  },
+  {
+    "revision": "00f4e123b73ae425afc83b316c17cdbe",
+    "url": "/jfdz12-fantasy-code-app/static/media/open.00f4e123.png"
+  },
+  {
+    "revision": "5c30946ca1cdc766a3833920a6a715f9",
+    "url": "/jfdz12-fantasy-code-app/static/media/spot.5c30946c.png"
+  }
+]);
